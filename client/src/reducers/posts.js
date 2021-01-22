3b8fa3bb8d7b0041f3b31fef6a320fa8134c869a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-22 16:29:36 -0600

    added more redux

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,9 @@
-export default const reducer = (posts = [], action) => {
+export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
         case 'CREATE':
-            return posts;
+            return [...posts, action.payload];
         default:
             return posts;
     }
--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://doug:blah@cluster0.mgvb8.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
